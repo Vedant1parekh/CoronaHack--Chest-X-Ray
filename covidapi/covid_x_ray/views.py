eb01194cc3341c1861da9ac9e6b990235d0e730a
Vedant1parekh <63174781+Vedant1parekh@users.noreply.github.com>
2021-07-10 10:36:29 +0530

    Updated Views.py
    Here I have added the functionality of saving and reading images to predict for detection of coronavirus.

--- covidapi/covid_x_ray/views.py
+++ covidapi/covid_x_ray/views.py
@@ -1,37 +1,49 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 import cv2
 from keras.models import load_model
-
-
+import base64
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+from .forms import ImageUploadForm
 # Create your views here.
+def handle_upload(f):
+    with open('img.jpg','wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 def index(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         return result(request)
     return render(request,'index.html')
 
 def result(request):
-    Answer = ''
-    ig = request.POST.get('img')
-    if not ig:
-        Answer = 'Invalid Image'
-    else:
-        a = []
-        lmodel = load_model("static/Covid_model_CNN.h5")
+    Answer="invalid_image"
+    form =ImageUploadForm(request.POST,request.FILES)
+    if form.is_valid():
+        a=[]
+        handle_upload(request.FILES['image'])
+        lmodel = load_model("C:/Users/Dell/Desktop/Covid/covidapi/Covid_X_ray/static/Covid_model_CNN.h5")
+        img_path="img.jpg"
         IMG_W = 224
-        IMG_H = 244
-        img_array = cv2.imread(ig, cv2.IMREAD_GRAYSCALE)
-        if img_array.ndim == 3:
-            img_array = cv2.resize(img_array,(IMG_W,IMG_H))/255.0
-            a.append(img_array)
-            r = lmodel.predict_classes(a[:1])
-            if r[0][0]==0:
-                Answer = 'NEGATIVE'
-            else:
-                Answer = 'POSITIVE'
-        else :
-            Answer = 'Image must be 3 dimension x-ray'
-
+        IMG_H = 224
+        img=np.array(cv2.imread(img_path))
+        img_array = np.array(cv2.resize(img,(IMG_W,IMG_H))/255.0)
+        a.append([img_array,0])
+        plt.imshow(img_array)
+        plt.show()
+        x_test=[]
+        for k,j in a:
+            x_test.append(k)
+        x_test=np.array(x_test)
+        r = lmodel.predict_classes(x_test)
+        print('------------------------')
+        print(r)
+        if r[0][0]==0:
+            Answer = 'NEGATIVE'
+        else:
+            Answer = 'POSITIVE'
+    
     context = {
         'Answer' : Answer
     }
-    return render(request,'result.html',context)
+    return render(request,'C:/Users/Dell\Desktop/Covid/covidapi/Covid_X_ray/templates/result.html',context)
