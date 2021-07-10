ddaa1945bdc6d8599faf2310f83f668168ebce5b
Vedant1parekh <63174781+Vedant1parekh@users.noreply.github.com>
2021-07-10 10:41:47 +0530

    Update urls.py

--- covidapi/covid_x_ray/urls.py
+++ covidapi/covid_x_ray/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from covid_x_ray import views
 urlpatterns = [
     path('', views.index),
-    path('index.html', views.index),
-    path('result.html', views.result),
+    path('result/', views.result),
 ]
